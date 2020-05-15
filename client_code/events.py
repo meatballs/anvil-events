8f66deb55abd2b3d4251ef2f3a15b0dee9c44ddf
Owen Campbell <owen.campbell@tanti.org.uk>
2020-05-15 10:34:32 +0100

    Add versioning

--- client_code/events.py
+++ client_code/events.py
@@ -15,21 +15,24 @@
 #    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #
 #   This program is published at https://github.com/meatballs/anvil-events
+__version__ = "0.1.0"
+
 _event_managers = {}
 
+
 class Manager:
-    """A class to handle events on components"""  
-    
+    """A class to handle events on components"""
+
     def __init__(self):
         self.events = {}
-        
+
     def register_handler(self, event, component, handler):
         """Regsiter the handler function for a given event on a given component"""
         if event not in self.events:
             self.events[event] = []
         self.events[event].append(component)
         component.set_event_handler(event, handler)
-      
+
     def propogate_event(self, event, up=True, down=True, **kwargs):
         """Raise a given event on all components for which it has been registered"""
         for component in self.events[event]:
@@ -39,10 +42,10 @@ class Manager:
             if down:
                 component.raise_event_on_children(event, **kwargs)
 
-            
+
 def register_manager(name, event_manager):
     _event_managers[name] = event_manager
-    
+
 
 def get_manager(name):
     return _event_managers[name]
